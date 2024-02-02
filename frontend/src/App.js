e09a93005d72c0adf8847d479c07e51768b4647a
Agampreet Kaur <122470353+kaurAgam@users.noreply.github.com>
2024-02-02 22:12:34 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { pdfjs } from "react-pdf";
+import PdfComp from "./PdfComp";
+
+pdfjs.GlobalWorkerOptions.workerSrc = 
+`//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [file, setFile] = useState("");
+  const [allImage, setAllImage] = useState(null);
+  const [pdfFile, setPdfFile] = useState(null);
+
+  useEffect(() => {
+    getPdf();
+  }, []);
+  const getPdf = async () => {
+    const result = await axios.get("http://localhost:5000/get-files");
+    console.log(result.data.data);
+    setAllImage(result.data.data);
+  };
+
+  const submitImage = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("file", file);
+    console.log(title, file);
+
+    const result = await axios.post(
+      "http://localhost:5000/upload-files",
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    console.log(result);
+    if (result.data.status === "ok") {
+      alert("Uploaded Successfully!!!");
+      getPdf();
+    }
+  };
+  const showPdf = (pdf) => {
+    // window.open(`http://localhost:5000/files/${pdf}`, "_blank", "noreferrer");
+    setPdfFile(`http://localhost:5000/files/${pdf}`)
+  };
+  return (
+    <div className="App text-light ">
+      <form className="formStyle  form-subtle-info" onSubmit={submitImage}>
+        <h4>Upload Pdf </h4>
+        <br />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <input
+          type="file"
+          class="form-control"
+          accept="application/pdf"
+          required
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <br />
+        <button class="btn btn-outline-info fw-bold" type="submit">
+          Submit
+        </button>
+      </form>
+      <div className="uploaded" id="uploaded-pdf">
+        <h4>Uploaded PDFs:</h4>
+        <div className="output-div">
+          {allImage == null
+            ? ""
+            : allImage.map((data) => {
+                return (
+                  <div className="inner-div">
+                    {data.title} ->
+                    <button
+                      className="btn m-2 btn-outline-light btn-sm"
+                      onClick={() => showPdf(data.pdf)}
+                    >
+                      Show Pdf
+                    </button>
+                  </div>
+                );
+              })}
+        </div>
+      </div>
+      <PdfComp pdfFile={pdfFile}/>
+    </div>
+  );
+}
+
+export default App;
